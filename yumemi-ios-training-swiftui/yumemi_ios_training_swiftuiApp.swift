546b7b0397de9cd313e960fa7eb4a3495e546b31
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-03-03 14:44:13 +0900

    first commit

---
+++ yumemi-ios-training-swiftui/yumemi_ios_training_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  yumemi_ios_training_swiftuiApp.swift
+//  yumemi-ios-training-swiftui
+//
+//  Created by 上條栞汰 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct yumemi_ios_training_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
